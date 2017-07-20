ef1ecf0ad66d8bf96fb2f8008b9b25064c62afef
pansp <547521091@qq.com>
2017-07-20 11:17:05 +0800

    修改依赖

--- com.linchuanedu.edu/com.linchuanedu.edu.main/src/main/java/com/linchuanedu/edu/MainApplication.java
+++ com.linchuanedu.edu/com.linchuanedu.edu.main/src/main/java/com/linchuanedu/edu/MainApplication.java
@@ -2,11 +2,15 @@ package com.linchuanedu.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+
+        System.out.println("服务器启动");
+    }
 }
