90191419baf08148cc88458c6c41aa76b69a0aab
pansp <547521091@qq.com>
2017-07-22 11:04:58 +0800

    add folders

--- com.linchuanedu.edu/com.linchuanedu.edu.api/src/main/java/com/linchuanedu/edu/api/controller/UserController.java
+++ com.linchuanedu.edu/com.linchuanedu.edu.api/src/main/java/com/linchuanedu/edu/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.linchuanedu.edu.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by pansp
@@ -9,6 +11,12 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller
+@RequestMapping(value = "/v1/edu/app/user")
 public class UserController {
+
+    @RequestMapping(value = "/signin",method = RequestMethod.POST, consumes = "application/json;charset=UTF-8", produces = "application/json;charset=UTF-8")
+    public void userSignIn() {
+
+    }
 
 }
